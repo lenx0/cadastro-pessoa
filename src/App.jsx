6c0b3365c251af889e3095f6f71612e4d2787687
AltoGiroLab <dev1@altogiro.net>
2022-07-07 17:18:47 -0300

    add contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,43 @@
-import React, { useState } from 'react';
-import './App.css';
-import data from '../mock.data.json'
+import React, { useState } from "react";
+import { nanoid } from 'nanoid';
+import "./App.css";
+import data from "../mock.data.json";
 
 function App() {
-
   const [contacts, setContacts] = useState(data);
+  const [addFormData, setAddFormData] = useState({
+    name: '',
+    address: '',
+    phoneNumber: '',
+    email: '',
+  });
+
+  const handleAddFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...addFormData};
+    newFormData[fieldName] = fieldValue;
+
+    setAddFormData(newFormData);
+  }
+
+  const handleAddFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newContact = {
+      id: nanoid(),
+      name: addFormData.name,
+      address: addFormData.address,
+      phoneNumber: addFormData.phoneNumber,
+      email: addFormData.email,
+    };
+
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  };
 
   return (
     <div className="app-container">
@@ -20,16 +53,49 @@ function App() {
         <tbody>
           {contacts.map((contact) => (
             <tr>
-            <td>{contact.name}</td>
-            <td>{contact.address}</td>
-            <td>{contact.phoneNumber}</td>
-            <td>{contact.email}</td>
-          </tr>
+              <td>{contact.name}</td>
+              <td>{contact.address}</td>
+              <td>{contact.phoneNumber}</td>
+              <td>{contact.email}</td>
+            </tr>
           ))}
         </tbody>
       </table>
+
+      <h2>Add a Contact</h2>
+      <form onSubmit={handleAddFormSubmit}>
+        <input
+          type="text"
+          name="name"
+          required="required"
+          placeholder="Enter a name..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="address"
+          required="required"
+          placeholder="Enter a address..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="text"
+          name="phoneNumber"
+          required="required"
+          placeholder="Enter a phone..."
+          onChange={handleAddFormChange}
+        />
+        <input
+          type="email"
+          name="email"
+          required="required"
+          placeholder="Enter a email..."
+          onChange={handleAddFormChange}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
