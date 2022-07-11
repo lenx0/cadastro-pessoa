a4b365d4adda3014d0a5952309d44615a7706239
AltoGiroLab <dev1@altogiro.net>
2022-07-11 09:53:21 -0300

    edit, save, cancel

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,27 @@
-import React, { useState } from "react";
-import { nanoid } from 'nanoid';
+import React, { useState, Fragment } from "react";
+import { nanoid } from "nanoid";
 import "./App.css";
 import data from "../mock.data.json";
+import ReadOnlyRow from "./components/ReadOnlyRow";
+import EditableRow from "./components/EditableRow";
 
-function App() {
+const App = () => {
   const [contacts, setContacts] = useState(data);
   const [addFormData, setAddFormData] = useState({
-    name: '',
-    address: '',
-    phoneNumber: '',
-    email: '',
+    name: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
   });
+
+  const [editFormData, setEditFormData] = useState({
+    name: "",
+    address: "",
+    phoneNumber: "",
+    email: "",
+  });
+
+  const [editContactId, setEditContactId] = useState(null);
 
   const handleAddFormChange = (event) => {
     event.preventDefault();
@@ -18,11 +29,23 @@ function App() {
     const fieldName = event.target.getAttribute("name");
     const fieldValue = event.target.value;
 
-    const newFormData = { ...addFormData};
+    const newFormData = { ...addFormData };
     newFormData[fieldName] = fieldValue;
 
     setAddFormData(newFormData);
-  }
+  };
+
+  const handleEditFormChange = (event) => {
+    event.preventDefault();
+
+    const fieldName = event.target.getAttribute("name");
+    const fieldValue = event.target.value;
+
+    const newFormData = { ...editFormData };
+    newFormData[fieldName] = fieldValue;
+
+    setEditFormData(newFormData);
+  };
 
   const handleAddFormSubmit = (event) => {
     event.preventDefault();
@@ -39,28 +62,89 @@ function App() {
     setContacts(newContacts);
   };
 
+  const handleEditFormSubmit = (event) => {
+    event.preventDefault();
+
+    const editedContact = {
+      id: editContactId,
+      name: editFormData.name,
+      address: editFormData.address,
+      phoneNumber: editFormData.phoneNumber,
+      email: editFormData.email,
+    };
+
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === editContactId);
+
+    newContacts[index] = editedContact;
+
+    setContacts(newContacts);
+    setEditContactId(null);
+  };
+
+  const handleEditClick = (event, contact) => {
+    event.preventDefault();
+    setEditContactId(contact.id);
+
+    const formValues = {
+      name: contact.name,
+      address: contact.address,
+      phoneNumber: contact.phoneNumber,
+      email: contact.email,
+    };
+
+    setEditFormData(formValues);
+  };
+
+  const handleCancelClick = () => {
+    setEditContactId(null);
+  };
+
+  const handleDeleteClick = (contactId) => {
+    const newContacts = [...contacts];
+
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+
+    newContacts.splice(index, 1);
+
+    setContacts(newContacts);
+  };
+
   return (
     <div className="app-container">
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Address</th>
-            <th>Phone Number</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact) => (
+      <form onSubmit={handleEditFormSubmit}>
+        <table>
+          <thead>
             <tr>
-              <td>{contact.name}</td>
-              <td>{contact.address}</td>
-              <td>{contact.phoneNumber}</td>
-              <td>{contact.email}</td>
+              <th>Name</th>
+              <th>Address</th>
+              <th>Phone Number</th>
+              <th>Email</th>
+              <th>Actions</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <Fragment>
+                {editContactId === contact.id ? (
+                  <EditableRow
+                    editFormData={editFormData}
+                    handleEditFormChange={handleEditFormChange}
+                    handleCancelClick={handleCancelClick}
+                  />
+                ) : (
+                  <ReadOnlyRow
+                    contact={contact}
+                    handleEditClick={handleEditClick}
+                    handleDeleteClick={handleDeleteClick}
+                  />
+                )}
+              </Fragment>
+            ))}
+          </tbody>
+        </table>
+      </form>
 
       <h2>Add a Contact</h2>
       <form onSubmit={handleAddFormSubmit}>
@@ -75,27 +159,27 @@ function App() {
           type="text"
           name="address"
           required="required"
-          placeholder="Enter a address..."
+          placeholder="Enter an addres..."
           onChange={handleAddFormChange}
         />
         <input
           type="text"
           name="phoneNumber"
           required="required"
-          placeholder="Enter a phone..."
+          placeholder="Enter a phone number..."
           onChange={handleAddFormChange}
         />
         <input
           type="email"
           name="email"
           required="required"
-          placeholder="Enter a email..."
+          placeholder="Enter an email..."
           onChange={handleAddFormChange}
         />
         <button type="submit">Add</button>
       </form>
     </div>
   );
-}
+};
 
 export default App;
